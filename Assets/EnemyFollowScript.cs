4eb6e7b7e8b2d7cd0092c76f41e185dbc5827f6a
Geoff Hill @ SAE Institute <g.hill@sae.edu>
2022-11-09 10:57:48 +0000

    Added Chase Volume Level for Enemy behaviour

---
+++ Assets/EnemyFollowScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowScript : MonoBehaviour
+{
+    public GameObject player;
+    public VolumeDetection volumeToMonitor;
+    public float speed = 1;
+    bool chasePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(volumeToMonitor.playerInsideVolume == true)
+        {
+            //Player is inside the volume because the bool over there is true
+            //glare at the player
+            transform.LookAt(player.transform.position);
+            //run after him
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/VolumeDetection.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeDetection : MonoBehaviour
+{
+    public Material on, off;
+    Renderer r;
+
+    public bool playerInsideVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player is in trigger");
+            playerInsideVolume = true;
+            r.material = on;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player is in trigger");
+            playerInsideVolume = false;
+            r.material = off;
+        }
+    }
+}
