2aac260853b6880bb17e6c2627e0a042923e4924
Geoff Hill @ SAE Institute <g.hill@sae.edu>
2022-11-23 12:03:59 +0000

    Added a simple FPS controller and a raycast based damage script

---
+++ Assets/ShooterScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    public GameObject myCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Bang bang");
+
+            //get the forward direction 10 units from the camera
+            Vector3 forward = myCamera.transform.TransformDirection(Vector3.forward) * 10;
+
+            //draw a line
+            Debug.DrawRay(myCamera.transform.position, forward, Color.cyan);
+
+            //save a raycast to store the hit data
+            RaycastHit hit;
+
+            //raycast out, if we hit something return it to hit
+            if (Physics.Raycast(myCamera.transform.position, forward, out hit))
+            {
+                Debug.Log("hit the " + hit.collider.gameObject.name);
+
+                //grab their Damage script
+                Damage enemyDamage =  hit.collider.gameObject.GetComponent<Damage>();
+                if (enemyDamage) //;check we got it and don't error
+                    enemyDamage.TakeDamage(); //call their damage script
+                
+            }
+
+        }
+
+    }
+}
